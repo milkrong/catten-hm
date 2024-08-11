2ef125cff6fe2968a19ef918bc686b4f8942a268
liurongkai <liurongkai@bytedance.com>
2024-08-11 14:39:27 +0800

    fix: type error

---
+++ i18n/en-US/global.ts
@@ -0,0 +1,13 @@
+const global = {
+  delete: 'Delete',
+  edit: 'Edit',
+  save: 'Save',
+  cancel: 'Cancel',
+  confirm: 'Confirm',
+  back: 'Back',
+  add: 'Add',
+  update: 'Update',
+  actions: 'Actions',
+};
+
+export default global;
